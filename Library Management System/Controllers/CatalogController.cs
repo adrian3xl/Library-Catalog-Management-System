4b7f0012b54766da578924a75807ba6232271ed9
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-07-18 16:54:34 -0500

    adding controllers

--- Library Management System/Controllers/CatalogController.cs
+++ Library Management System/Controllers/CatalogController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Library_Management_System.Contracts;
+using Library_Management_System.Data;
+using Library_Management_System.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,27 +13,50 @@ namespace Library_Management_System.Controllers
 {
     public class CatalogController : Controller
     {
-        private readonly ILibraryRecordRepository libraryRecordRepo;
+        private readonly ILibraryRecordRepository _libraryRecordRepo;
         private readonly ILibraryEmployeeRepository _libraryEmployeeRepo;
         private readonly IAuthorRepository _authorRepo;
         private readonly IPublisherRepository _publisherRepo;
         private readonly IMapper _mapper;
         private readonly ICatalogRepository _repo;
 
+        CatalogController(ILibraryRecordRepository libraryRecordRepo,
+            ILibraryEmployeeRepository libraryEmployeeRepo,
+            IAuthorRepository authorRepo,
+            IPublisherRepository publisherRepo, 
+            ICatalogRepository repo,
+            IMapper mapper)
+      
+        {
+             _repo = repo;
+            _libraryRecordRepo = libraryRecordRepo;
+            _libraryEmployeeRepo = libraryEmployeeRepo;
+            _authorRepo = authorRepo;
+            _publisherRepo = publisherRepo;
+            _mapper = mapper;
 
+        } 
 
 
 
         // GET: CatalogController
         public ActionResult Index()
         {
-            return View();
+            var catalog = _repo.FindAll().ToList();
+            var model = _mapper.Map<List<Catalog>, List<CatalogVM>>(catalog);
+            return View(model);
         }
 
         // GET: CatalogController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            if (!_repo.IsExist(id))
+            {
+                return NotFound();
+            }
+            var catalog = _repo.FindById(id);
+            var model = _mapper.Map<CatalogVM>(catalog);
+            return View(model);
         }
 
         // GET: CatalogController/Create
@@ -43,58 +68,120 @@ namespace Library_Management_System.Controllers
         // POST: CatalogController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(CatalogVM model)
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                var catalog = _mapper.Map<Catalog>(model);
+                var isSucess = _repo.Create(catalog);
+
+                if (!isSucess)
+                {
+                    ModelState.AddModelError("", "Something went wrong...");
+                    return View(model);
+                }
+
+
+
+
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                ModelState.AddModelError("", "Something went wrong...");
+                return View(model);
             }
         }
 
         // GET: CatalogController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            if (!_repo.IsExist(id))
+            {
+                return NotFound();
+            }
+            var catalog= _repo.FindById(id);
+            var model = _mapper.Map<CatalogVM>(catalog);
+            return View(model);
         }
 
         // POST: CatalogController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, CatalogVM model)
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+                var catalog = _mapper.Map<Catalog>(model);
+                var isSucess = _repo.Update(catalog);
+                if (!isSucess)
+                {
+                    ModelState.AddModelError("", "Something went wrong...");
+                    return View(model);
+                }
+
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                ModelState.AddModelError("", "Something went wrong...");
+                return View(model);
             }
         }
 
         // GET: CatalogController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var catalog = _repo.FindById(id);
+            if (catalog == null)
+            {
+                return NotFound();
+            }
+            var isSucess = _repo.Delete(catalog);
+            if (!isSucess)
+            {
+
+                return BadRequest();
+            }
+
+            return RedirectToAction(nameof(Index));
         }
 
         // POST: CatalogController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, CatalogVM model)
         {
             try
             {
+                var catalog = _repo.FindById(id);
+                if (catalog == null)
+                {
+                    return NotFound();
+                }
+                var isSucess = _repo.Delete(catalog);
+                if (!isSucess)
+                {
+
+                    return View(model);
+                }
+
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(model);
             }
+
         }
     }
 }
