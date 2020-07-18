aa14e324f521259ab8782f15265a0c0af9688ca0
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-07-18 15:59:47 -0500

    added more stuff

---
+++ Library Management System/Controllers/CatalogController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Library_Management_System.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_System.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly ILibraryRecordRepository libraryRecordRepo;
+        private readonly ILibraryEmployeeRepository _libraryEmployeeRepo;
+        private readonly IAuthorRepository _authorRepo;
+        private readonly IPublisherRepository _publisherRepo;
+        private readonly IMapper _mapper;
+        private readonly ICatalogRepository _repo;
+
+
+
+
+
+        // GET: CatalogController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CatalogController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CatalogController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CatalogController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CatalogController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CatalogController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CatalogController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CatalogController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Library Management System/Controllers/LibraryRecordController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Library_Management_System.Contracts;
+using Library_Management_System.Data;
+using Library_Management_System.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_System.Controllers
+{
+    public class LibraryRecordController : Controller
+    {
+
+        private readonly ILibraryRecordRepository _repo;
+        private readonly ILibraryEmployeeRepository _libraryEmployeeRepo;
+        private readonly IAuthorRepository _authorRepo;
+        private readonly IPublisherRepository _publisherRepo;
+        private readonly ICatalogRepository _catalogRepo;
+        private readonly IMapper _mapper;
+
+
+        LibraryRecordController(ILibraryRecordRepository repo, ILibraryEmployeeRepository libraryEmployeeRepo, IPublisherRepository publisherRepo,IAuthorRepository authorRepo
+            , ICatalogRepository catalogRepo, IMapper mapper)
+        {
+
+            _repo = repo;
+            _catalogRepo = catalogRepo;
+            _libraryEmployeeRepo = libraryEmployeeRepo;
+            _authorRepo = authorRepo;
+            _publisherRepo = publisherRepo;
+            _mapper = mapper;
+        }
+
+
+
+        // GET: LibraryRecordController
+        public ActionResult Index()
+        {
+            var libraryRecord = _repo.FindAll().ToList();
+            var model = _mapper.Map<List<LibraryRecord>, List<LibraryRecordVM>>(libraryRecord);
+            return View(model);
+        }
+
+        // GET: LibraryRecordController/Details/5
+        public ActionResult Details(int id)
+        {
+            if (!_repo.IsExist(id))
+            {
+                return NotFound();
+            }
+            var libraryRecord = _repo.FindById(id);
+            var model = _mapper.Map<LibraryRecordVM>(libraryRecord);
+            return View(model);
+        }
+
+        // GET: LibraryRecordController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LibraryRecordController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(LibraryRecordVM model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                var libraryRecord = _mapper.Map<LibraryRecord>(model);
+                var isSucess = _repo.Create(libraryRecord);
+
+                if (!isSucess)
+                {
+                    ModelState.AddModelError("", "Something went wrong...");
+                    return View(model);
+                }
+
+
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Something went wrong...");
+                return View(model);
+            }
+
+        }
+
+        // GET: LibraryRecordController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (!_repo.IsExist(id))
+            {
+                return NotFound();
+            }
+            var libraryRecord = _repo.FindById(id);
+            var model = _mapper.Map<LibraryRecordVM>(libraryRecord);
+            return View(model);
+        }
+
+        // POST: LibraryRecordController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, LibraryRecordVM model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+                var libraryRecord = _mapper.Map<LibraryRecord>(model);
+                var isSucess = _repo.Update(libraryRecord);
+                if (!isSucess)
+                {
+                    ModelState.AddModelError("", "Something went wrong...");
+                    return View(model);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Something went wrong...");
+                return View(model);
+            }
+        }
+
+        // GET: LibraryRecordController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var libraryRecord = _repo.FindById(id);
+            if (libraryRecord == null)
+            {
+                return NotFound();
+            }
+            var isSucess = _repo.Delete(libraryRecord);
+            if (!isSucess)
+            {
+
+                return BadRequest();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: LibraryRecordController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, LibraryRecordVM model)
+        {
+            try
+            {
+                var libraryRecord = _repo.FindById(id);
+                if (libraryRecord == null)
+                {
+                    return NotFound();
+                }
+                var isSucess = _repo.Delete(libraryRecord);
+                if (!isSucess)
+                {
+
+                    return View(model);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(model);
+            }
+
+        }
+    }
+}
