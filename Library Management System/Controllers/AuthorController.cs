f6be57ecd751c13cbbacc0c9e8e1093c4976e82a
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-07-17 19:58:49 -0500

    update

--- Library Management System/Controllers/AuthorController.cs
+++ Library Management System/Controllers/AuthorController.cs
@@ -11,40 +11,40 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Library_Management_System.Controllers
 {
-    public class AuthorsController : Controller
+    public class AuthorController : Controller
     {
-            private readonly IAuthorRepository _repo;
-            private readonly IMapper _mapper;
+        private readonly IAuthorRepository _repo;
+        private readonly IMapper _mapper;
 
 
-            AuthorsController(IAuthorRepository repo, IMapper mapper)
-            {
+        AuthorController(IAuthorRepository repo, IMapper mapper)
+        {
 
-                _repo = repo;
-                _mapper = mapper;
-            }
+            _repo = repo;
+            _mapper = mapper;
+        }
 
-            // GET: AuthorsController
-            public ActionResult Index()
-            {
-                var Authors = _repo.FindAll().ToList();
-                var model = _mapper.Map<List<Author>, List<AuthorVM>>(Authors);
-                return View(model);
-            }
+        // GET: AuthorController
+        public ActionResult Index()
+        {
+            var Authors = _repo.FindAll().ToList();
+            var model = _mapper.Map<List<Author>, List<AuthorVM>>(Authors);
+            return View(model);
+        }
 
-        // GET: AuthorsController/Details/5
+        // GET: AuthorController/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: AuthorsController/Create
+        // GET: AuthorController/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: AuthorsController/Create
+        // POST: AuthorController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(IFormCollection collection)
@@ -59,13 +59,13 @@ namespace Library_Management_System.Controllers
             }
         }
 
-        // GET: AuthorsController/Edit/5
+        // GET: AuthorController/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: AuthorsController/Edit/5
+        // POST: AuthorController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, IFormCollection collection)
@@ -80,13 +80,13 @@ namespace Library_Management_System.Controllers
             }
         }
 
-        // GET: AuthorsController/Delete/5
+        // GET: AuthorController/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: AuthorsController/Delete/5
+        // POST: AuthorController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
