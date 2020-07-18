8581f5eec47325f3dc9823ed7f5ea0443788451d
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-07-18 17:54:33 -0500

    login functionality

---
+++ Library Management System/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Library_Management_System.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Library_Management_System.Areas.Identity.IdentityHostingStartup))]
+namespace Library_Management_System.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
