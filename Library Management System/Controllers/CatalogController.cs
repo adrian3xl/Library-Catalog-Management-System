908344242d57235826762e6ef534803901fd1433
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-07-20 20:34:38 -0500

    update(witch craft)

--- Library Management System/Controllers/CatalogController.cs
+++ Library Management System/Controllers/CatalogController.cs
@@ -68,21 +68,21 @@ namespace Library_Management_System.Controllers
 
             var authorTypeItems = author.Select(q => new SelectListItem
             {
-                Text = q.Firstname, qLastname,
+                Text = "$(q.Firstname}{q.Lastname}",
                 Value = q.Id.ToString()
             });
 
             var publisherTypeItems = publisher.Select(q => new SelectListItem
             {
-                Text = q.Firstname,
-                q.Lastname,
+                Text ="$(q.Firstname}{q.Lastname}",
+             
                 Value = q.Id.ToString()
             });
 
             var model = new CatalogVM
             {
-                publisher = publisherTypeItems,
-                author = authorTypeItems
+                Publishers = publisherTypeItems,
+                Authors = authorTypeItems
 
             };
             return View(model);
