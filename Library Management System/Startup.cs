2d7fe5113e9489137285e6cb042d669cf547aaf5
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-07-23 14:05:05 -0500

    update

--- Library Management System/Startup.cs
+++ Library Management System/Startup.cs
@@ -47,13 +47,18 @@ namespace Library_Management_System
             services.AddAutoMapper(typeof(Maps));
 
             services.AddDefaultIdentity<IdentityUser>()
+
+                    .AddRoles<IdentityRole>()
+
+
+
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,UserManager<IdentityUser> userManager,RoleManager<IdentityRole> roleManager)
         {
             if (env.IsDevelopment())
             {
